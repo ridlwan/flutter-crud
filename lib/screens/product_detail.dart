3da5a79a93782f71578e4099f022e5863499ac70
Fatkhur Ridlwan <fridlwan@gmail.com>
2022-05-31 08:03:13 +0700

    Initial commit

---
+++ lib/screens/product_detail.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import 'edit_product.dart';
+
+class ProductDetail extends StatelessWidget {
+  final Map product;
+
+  ProductDetail({required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Product Detail')),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              child: Image.network(product['image_url']),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(product['price'], style: TextStyle(fontSize: 22),),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => EditProduct(product: product,)));
+                        },
+                        child: Icon(Icons.edit)),
+                      Icon(Icons.delete)
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Text(product['description'])
+          ],
+        ),
+      ),
+    );
+  }
+}
